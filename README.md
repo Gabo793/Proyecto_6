52d46bd74b1288d9d6f4b63bdf1f079754253458
Gabo793 <gabo_aap79@hotmail.com>
2024-09-04 17:20:40 -0500

    Version 1 del Proyecto_6

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Proyecto_6
 Proyecto de finalización Sprint 6
+
+El programa nos ayuda a:
+1: Visualizar un histograma en base al kilometraje de los vehiculos a la venta a travez de un boton.
+2: Visualizar un diagrama de dispersion, que nos muestra como varia el precio en relacion al kilometraje del vehiculo.
+
+
---
+++ app.py
@@ -0,0 +1,47 @@
+#Se importa librerias necesarias 
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+#Se importa el DataFrame necesario
+car_data = pd.read_csv("C:/Users/dicop/Desktop/Data/Data/Data/Data_Scientist/Sprint_6/Proyecto 6/vehicles_us.csv")
+
+#Titulo de la pagina web
+st.header("Proyecto 6") 
+
+#Creacion del boton para construir el histograma
+hist_button = st.button("Construir histograma")
+
+#Creacion del boton de verificacion 
+build_scat = st.checkbox("Construir un diagrama de dispersion")
+
+#Configuracion del boton "Construir histograma"
+if hist_button:
+    
+    #Mensaje al presionar el boton
+    st.write("Creacion de un histograma para el conjunto de datos de anuncio de ventas de coche") 
+    
+    #Crear un histograma
+    fig = px.histogram(car_data, x = "odometer")
+    
+    #Se muestra un grafico plotly interactivo
+    st.plotly_chart(fig, use_conteiner_width=True)
+
+
+#Configuracion de la casilla "Construir un diagrama de dispersion"    
+elif build_scat:
+    
+     #Mensaje al presionar el boton
+    st.write("Creacion de un diagrama de dispersion para el conjunto de datos de anuncio de ventas de coche") 
+    
+    #Crear un diagrama de dispersion
+    fig_4 = px.scatter(car_data, x = "odometer", y = "price")
+    
+    #Se muestra un grafico plotly interactivo
+    st.plotly_chart(fig_4, use_conteiner_width=True)
+
+
+    
+
+
+
