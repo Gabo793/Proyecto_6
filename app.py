e798328cd4ca1a04209cf5282523356a47caca3c
Gabriel Arias <gabo_aap79@hotmail.com>
2024-09-04 23:01:41 -0500

    Quinta revision Proyecto 6

--- app.py
+++ app.py
@@ -1,78 +1,85 @@
-#Se importa librerias necesarias 
+# Se importa librerias necesarias
 import streamlit as st
 import pandas as pd
 import plotly_express as px
 
-#Se importa el DataFrame necesario
+# Se importa el DataFrame necesario
 car_data = pd.read_csv("vehicles_us.csv")
 
-#Titulo de la pagina web
+# Titulo de la pagina web
 st.header("Comparacion de vehiculos usados por kilometraje")
-st.header("Proyecto 6") 
+st.header("Proyecto 6")
 
-#Creacion del boton para construir el histograma
+# Creacion del boton para construir el histograma
 hist_button = st.button("Construir histograma")
 hist_scat = st.button("Construir un diagrama de dispersion")
 
-#Creacion del boton de verificacion 
+# Creacion del boton de verificacion
 build_hist = st.checkbox("Construir un histograma")
 build_scat = st.checkbox("Construir un diagrama de dispersion")
 
-
-#Configuracion del boton "Construir histograma"
+# Configuracion del boton "Construir histograma"
 if hist_button:
-    
-    #Mensaje al presionar el boton
-    st.write("Creacion de un histograma para el conjunto de datos de anuncio de ventas de coche") 
-    
-    #Crear un histograma
-    fig = px.histogram(car_data, x = "odometer")
-    
-    #Se muestra un grafico plotly interactivo
+
+    # Mensaje al presionar el boton
+    st.write(
+        "Creacion de un histograma para el conjunto de datos de anuncio de ventas de coche")
+
+    # Crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # Se muestra un grafico plotly interactivo
     st.plotly_chart(fig, use_conteiner_width=True)
-    
-    
-#Configuracion del boton "Construir un diagrama de dispersion"
+
+
+# Configuracion del boton "Construir un diagrama de dispersion"
 elif hist_scat:
-    
-    #Mensaje al presionar el boton
-    st.write("Creacion de un diagrama de dispersion para el conjunto de datos de anuncio de ventas de coche") 
-    
-    #Crear un diagrama de dispersion
-    fig_2 = px.scatter(car_data, x = "odometer", y = "price")
-    
-    #Se muestra un grafico plotly interactivo
+
+    # Mensaje al presionar el boton
+    st.write("Creacion de un diagrama de dispersion para el conjunto de datos de anuncio de ventas de coche")
+
+    # Crear un diagrama de dispersion
+    fig_2 = px.scatter(car_data, x="odometer", y="price")
+
+    # Se muestra un grafico plotly interactivo
     st.plotly_chart(fig_2, use_conteiner_width=True)
-    
-    
-#Configuracion de la casilla "Construir un histograma"    
+
+# Configuracion para cuando se seleccionen ambas casillas
+elif build_hist and build_scat:
+
+    # Mensaje al seleccionar ambas casillas
+    st.write("Creacion de un histograma y un diagrama de dispersion para el conjunto de datos de anuncion de ventas de coches")
+
+    # Se crea un histograma y un diagrama de dispersion
+    fig_5 = px.histogram(car_data, x="odometer")
+    fig_6 = px.scatter(car_data, x="odometer", y="price")
+
+    # Se muestran los 2 graficos
+    st.plotly_chart(fig_5, use_container_width=True)
+    st.plotly_chart(fig_6, use_container_width=True)
+
+
+# Configuracion de la casilla "Construir un histograma"
 elif build_hist:
-    
-    #Mensaje al presionar el boton
-    st.write("Creacion de un diagrama de dispersion para el conjunto de datos de anuncio de ventas de coche") 
-    
-    #Crear un histograma
-    fig_3 = px.histogram(car_data, x = "odometer")
-    
-    #Se muestra un grafico plotly interactivo
+
+    # Mensaje al presionar el boton
+    st.write(
+        "Creacion de un histograma para el conjunto de datos de anuncio de ventas de coche")
+
+    # Crear un histograma
+    fig_3 = px.histogram(car_data, x="odometer")
+
+    # Se muestra un grafico plotly interactivo
     st.plotly_chart(fig_3, use_conteiner_width=True)
-    
-#Configuracion de la casilla "Construir un diagrama de dispersion"    
+
+# Configuracion de la casilla "Construir un diagrama de dispersion"
 elif build_scat:
-    
-    #Mensaje al presionar el boton
-    st.write("Creacion de un diagrama de dispersion para el conjunto de datos de anuncio de ventas de coche") 
-    
-    #Crear un diagrama de dispersion
-    fig_4 = px.scatter(car_data, x = "odometer", y = "price")
-    
-    #Se muestra un grafico plotly interactivo
+
+    # Mensaje al presionar el boton
+    st.write("Creacion de un diagrama de dispersion para el conjunto de datos de anuncio de ventas de coche")
+
+    # Crear un diagrama de dispersion
+    fig_4 = px.scatter(car_data, x="odometer", y="price")
+
+    # Se muestra un grafico plotly interactivo
     st.plotly_chart(fig_4, use_conteiner_width=True)
-    
-
-
-
-    
-
-
-
